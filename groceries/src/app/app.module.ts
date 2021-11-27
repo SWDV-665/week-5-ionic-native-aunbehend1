5f5e3691049e2bbbc1b1f04cd98f462ccd9ae236
aunbehend <67622525+aunbehend@users.noreply.github.com>
2021-11-26 23:10:28 -0700

    app didn't get pushed
    Has week6 changes still works in progress. Week 5 didn't get pushed for some reason, trying to push the files for at least some points

---
+++ groceries/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { GroceriesServiceProvider } from '../app/groceries-service.service'
+import { InputDialogServiceProvider } from './input-dialog-service.service';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [SocialSharing, GroceriesServiceProvider, InputDialogServiceProvider,
+             { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
